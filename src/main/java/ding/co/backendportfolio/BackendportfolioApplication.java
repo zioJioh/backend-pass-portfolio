b615dee0d246585f30bf24271398a662c7456519
gioJioh <sjgio0515@naver.com>
2025-09-16 02:40:54 +0900

    workflow test commit

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
+        // gg
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
